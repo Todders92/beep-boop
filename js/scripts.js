8d2fac68b5a4c62b424b496cc7a1f87001f75264
Student <example@example.com>
2020-01-24 15:42:31 -0800

    add nested for loop to potentially help with conditionals

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,26 @@
 $(document).ready(function() {
   $("form#form").submit(function(event) {
     event.preventDefault();
-    var numbers = parseInt($("input#input-number").val());
+    var numbers =  parseInt($("input.input-number").val());
+    var newArray = [];
+    newArray.push(numbers);
+    newArray.forEach(function() {
     console.log(numbers);
-    if (isNaN(numbers)) {
-      alert("Make sure you are entering a number")
-    } else 
-    for (var index = 0; index <= numbers; index += 1) {
-      if (index.toString().includes("3")) {
-        numbers[index] = "I'm sorry, Dave. I'm afraid I can't do that.";
-      } else if (index.toString().includes("2")) {
-        numbers[index] = "Boop!";
-      } else if (index.toString().includes("1")) {
-        numbers[index] = "Beep!";
-      }
-          $("#results ul").append("<li>" + index +"</li>")
-          console.log(numbers)
-      };
+    console.log(newArray)
+    // if (isNaN(numbers)) {
+    //   alert("Make sure you are entering a number")
+    // } else 
+      for (var index = 0; index <= numbers; index += 1) {
+        if (index.toString().includes("3")) {
+          numbers[index] = "///";
+        } else if (index.toString().includes("2")) {
+          numbers[index] = "-";
+        } else if (index.toString().includes("1")) {
+          numbers[index] = ".....";
+        }
+            $("#results ul").append("<li>" + index +"</li>")
+        };
+      });
       $("#results").show();
   });
 });
