67a3a4556a6cebcddc897af2fca36e0752f0ee2f
Student <example@example.com>
2020-01-24 09:41:42 -0800

    complete requirements of first two specs

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,16 @@
 // User logic below this line:
 $(document).ready(function() {
   $("form#form").submit(function(event) {
-    event.preventDefault();
+      event.preventDefault();
+    var userInput = parseInt($("input#input-number").val());
+    console.log(userInput);
+    if (isNaN(userInput)) {
+      alert("Make sure you are entering a number")
+    } else
+      for (var i = 0; i <= userInput; i += 1){
+        $("#results ul").append("<li>" + i +"</li>")
+      }
+    
     $("#results").show();
   });
 });
