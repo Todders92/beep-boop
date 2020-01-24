165700a5388c199bd8188531fa867b1b9983fc1f
Student <example@example.com>
2020-01-24 13:47:01 -0800

    separate business and user logic to help bugfix

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,19 @@
 // Business logic below this line:
-var phrase1 = "im sorry jon"
+var numberFun = function(number) {
+  var numbers =[];
+  for (var i = 0; i<= number; i += 1) {
+    numbers.push(i);
+  }
+  for (var i = 0; i <= numbers; i += 1){
+    console.log(numbers)
+  }
+};
 // User logic below this line:
 $(document).ready(function() {
   $("form#form").submit(function(event) {
     event.preventDefault();
-    var numbers = parseInt($("input#input-number").val().split());
-    console.log(numbers);
-    if (isNaN(numbers)) {
-      alert("Make sure you are entering a number")
-    } else 
-      for (var i = 0; i <= numbers; i ++){
-          if (i === 3) {
-            var i = phrase1
-          }
-          $("#results ul").append("<li>" + i +"</li>")
-      };
-      $("#results").show();
+    var inputNumber = parseInt($("input#input-number").val());
+    numberFun(number);
+    $("#results").show();
   });
 });
