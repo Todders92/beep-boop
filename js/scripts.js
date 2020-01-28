d84469fef40cff4a34888851901e9baf3280e8d4
Todd Walraven <cod4247@yahoo.com>
2020-01-27 22:45:48 -0800

    change variables for for loop for clearer meaning

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 // Business logic below this line:
 
 // user logic below this line:
+var makeItem = function(item) {
+  $("results ul").append("<li>" + item + "</li>")
+}
 $(document).ready(function() {
   $("form#form").submit(function(event) {
     event.preventDefault();
@@ -15,16 +18,20 @@ $(document).ready(function() {
     //   alert("Make sure you are entering a number")
     // } else 
       for (var index = 0; index <= numbers; index += 1) {
-        if (index.toString().includes("3") === true) {
-          numbers[index] = "///";
-        } else if (index.toString().includes("2") === true) {
-          numbers[index] = "-";
-        } else if (index.toString().includes("1") === true) {
-          numbers[index] = ".....";
+        if (index.toString().includes("3")) {
+          newArray[index] = "///";
+        } else if (index.toString().includes("2")) {
+          newArray[index] = "-";
+        } else if (index.toString().includes("1")) {
+          newArray[index] = ".....";
+        } else {
+
         }
-            $(".results ul").append("<li>" + index +"</li>")
-        };
+      };
       });
-      $("#results").show();
+      for (i = 0; i < newArray.length; i ++) {
+        makeItem(newArray[i])
+        $("#results").show();
+      }
   });
 });
