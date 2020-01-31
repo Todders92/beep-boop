3c8fef7af07f0b12c905791bc8b9d919d63f6b39
Todd Walraven <cod4247@yahoo.com>
2020-01-30 18:03:11 -0800

    separate functions from each other

--- js/scripts.js
+++ js/scripts.js
@@ -2,32 +2,24 @@
 
 // user logic below this line:
 var makeItem = function(item) {
-  $("results ul").append("<li>" + item + "</li>")
+  $(".results ul").append("<li>" + item + "</li>")
 }
 $(document).ready(function() {
   $("form#form").submit(function(event) {
     event.preventDefault();
-    var numbers =  $("input.input-number").val();
-    var numbers = parseInt(numbers);
-    var newArray = [];
-    console.log(numbers);
-    // if (isNaN(numbers)) {
-    //   alert("Make sure you are entering a number")
-    // } else 
-      for (var index = 0; index <= numbers; index += 1) {
-        if (index.toString().includes("3")) {
-          newArray[index] = "///"
-        } else if (index.toString().includes("2")) {
-          newArray[index] = "-"
-        } else if (index.toString().includes("1")) {
-          newArray[index] = "....."
-        } else {
-          newArray[index] = index
-        }
-      };
-      for (i = 0; i < newArray.length; i ++) {
-        makeItem(newArray[i])
-        $("#results").show();
+    var numbers = parseInt($("input.input-number").val());
+    for (var i = 0; i<= numbers; i ++) {
+      if(i.toString().includes("3")) {
+        makeItem("I'm Sorry John. I'm afraid I can't do that.");
+      } else if (i.toString().includes("2")) {
+        makeItem("boop")
+      } else if (i.toString().includes("1")) {
+        makeItem("beep")
+      } else{
+        makeItem(i)
       }
+    };
+    
   });
+  $(".results").show();
 });
